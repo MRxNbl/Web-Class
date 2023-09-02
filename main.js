3a3ddad7724d8c1b34ad62d67ebd5918b38cd9fd
MG6__π788 <93661535+MRxNbl@users.noreply.github.com>
2023-09-02 23:34:53 +0700

    Add files via upload
    This For My Web Class

---
+++ main.js
@@ -0,0 +1,173 @@
+
+
+function DKV1(){
+  Swal.fire({
+    title: 'Belum Tersedia',
+    background: '#252523',
+    color: '#FFFFFF'
+  })
+  
+  
+}
+function DKV2(){}
+
+function DKV3(){
+  Swal.fire({
+    title: 'Belum Tersedia',
+    background: '#252523',
+    color: '#FFFFFF'
+  })}
+
+function openig()
+{
+  window.open("https://instagram.com/dkv2_smknet?igshid=MzRlODBiNWFlZA==")
+}
+
+function openmessaage()
+{
+  window.open("")
+}
+
+function openfeedback()
+{
+  window.open("mailto:nabilsasbianto89@gmail.com")
+}
+
+function wa(){
+  window.open("https://api.whatsapp.com/send?phone=+6283119436816&text=saya%20order%20Premium%20user")
+}
+
+function opensmk()
+{
+  window.open("https://maps.app.goo.gl/FTyfujaUckHfHjQC6")
+}
+
+
+function openstudent()
+{
+  window.open("Student.html")
+}
+
+function openhome()
+{
+  window.open("index.html")
+}
+
+function share()
+{
+  window.open("whatsapp://send?text=saya baru saja melihat website kelas XI DKV 2 SMKN Tutur%0Ahttps://dkv-2-beta.mrxnbl.repl.co/%0Ajangan lupa untuk mengunjunginya juga 🤩")
+}
+
+function opentxt()
+{
+  window.open("info.txt")
+}
+
+
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+
+
+
+
+
+
+
+
+
+
+function effect(){
+  
+  document.getElementById("load").style.display = "none"
+  
+};
+
+
+const menu = document.querySelector(".menu");
+const toggle = document.querySelector(".toggle");
+toggle.addEventListener("click", () => {
+  menu.classList.toggle("active");
+});
+
+
+
+
+
+
+document.getElementById('content').onclick = 
+effect();
+
+
+
+function pla() {
+var audio = new Audio('bgm.mp3');audio.play();}  
+     async function reaksi(){
+ };
+ 
+ 
+ 
+ 
+ async function Onnline(){
+
+    await swal.fire({
+
+      title: 'You Are Online',
+
+      text:'You can continue',
+
+      background: 'black',
+      
+      position: 'top-end',
+      
+      color: '#FFFFFF',
+      
+      showConfirmButton: false,
+      
+      showClass: {
+    popup: 'animate__animated animate__fadeInDown'
+  },
+  hideClass: {
+    popup: 'animate__animated animate__fadeOutUp'
+  },
+  
+  timer: 2500,
+
+    });
+
+  }
+
+  
+
+  async function Onffline(){
+
+    await swal.fire({
+
+      title:'You Are Offline',
+
+      text:'please check your connection',
+
+color: '#FFFFFF',
+
+      background: 'black',
+      
+      showConfirmButton: false,
+      
+      showClass: {
+          popup: 'animate__animated animate__fadeInDown'
+        },
+        hideClass: {
+          popup: 'animate__animated animate__fadeOutUp'
+        },
+        
+        timer: 2500,
+
+    });
+
+  }
